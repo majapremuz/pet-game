2812a9eadcb3a8aa02278ba8ceedd3d2db7fd9f8
majapremuz <majchi@windowslive.com>
2024-11-04 21:10:56 +0100

    dorada na game buttons

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -4,6 +4,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
 import { Router } from '@angular/router';
 
+type StatName = 'hunger' | 'fatigue' | 'purity' | 'attention';
+type StatColorName = 'hungerColor' | 'fatigueColor' | 'purityColor' | 'attentionColor';
+
+interface StatProperties {
+  hungerValue: number;
+  fatigueValue: number;
+  purityValue: number;
+  attentionValue: number;
+}
+
 @Component({
   selector: 'app-game',
   templateUrl: 'game.page.html',
@@ -11,11 +21,18 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule],
 })
-export class GamePage {
+export class GamePage implements StatProperties  {
   hungerValue: number = 100;
   fatigueValue: number = 100;
   purityValue: number = 100;
   attentionValue: number = 100;
+
+  hungerColor: string = '#d3ba77';
+  fatigueColor: string = '#d3ba77';
+  purityColor: string = '#d3ba77';
+  attentionColor: string = '#d3ba77';
+
+  value: number = 100;
   decreaseInterval: any;
   points: number = 0; 
   level: number = 0; 
@@ -27,6 +44,17 @@ export class GamePage {
   audio: HTMLAudioElement;
   selectedDogImage: string = '';
   showHeart: boolean = false;
+  nextFeedTime: Date | undefined;
+
+  currentStatValue: number = this.hungerValue;
+currentStatColor: string = this.getStatusColor(this.hungerValue);
+
+   // New properties for intervals and next action times
+   hungerNextAction: Date | undefined;
+   fatigueNextAction: Date | undefined;
+   purityNextAction: Date | undefined;
+   attentionNextAction: Date | undefined;
+ 
 
   constructor(
     private cdRef: ChangeDetectorRef,
@@ -40,34 +68,164 @@ export class GamePage {
   }
 
   ngOnInit() {
+    this.setInitialActionTimes();
     this.startStatusDecreasing();
     this.updateColorGradually();
   }
 
-  startStatusDecreasing() {
-    setInterval(() => this.decreaseStat('hunger', 10), 6 * 60 * 60 * 1000); // svaki 6 sati
-    setInterval(() => this.decreaseStat('fatigue', 15), 24 * 60 * 60 * 1000); // svaka 24 sata
-    setInterval(() => this.decreaseStat('purity', 20), 18 * 60 * 60 * 1000); // svaki 18 sati
-    setInterval(() => this.decreaseStat('attention', 5), 8 * 60 * 60 * 1000); // svaki 8 sati
+   // Sets initial times for each action
+   setInitialActionTimes() {
+    const now = new Date();
+    this.hungerNextAction = new Date(now.getTime() + this.getRandomInterval(6, 12)); // 6-12 hours
+    this.fatigueNextAction = new Date(now.getTime() + 24 * 60 * 60 * 1000); // 24 hours
+    this.purityNextAction = new Date(now.getTime() + 18 * 60 * 60 * 1000); // 18 hours
+    this.attentionNextAction = new Date(now.getTime() + 8 * 60 * 60 * 1000); // 8 hours
   }
 
-  decreaseStat(stat: string, decrement: number) {
+  // Returns a random interval within the given hour range
+  getRandomInterval(minHours: number, maxHours: number): number {
+    const randomHours = Math.floor(Math.random() * (maxHours - minHours + 1) + minHours);
+    return randomHours * 60 * 60 * 1000;
+  }
+
+  // Decrease stats gradually at specified intervals
+  startStatusDecreasing() {
+    setInterval(() => {
+      this.decreaseStat('hunger', 10);
+    }, 6 * 60 * 60 * 1000); 
+  
+    setInterval(() => {
+      this.decreaseStat('fatigue', 10);
+    }, 24 * 60 * 60 * 1000);
+  
+    setInterval(() => {
+      this.decreaseStat('purity', 15);
+    }, 18 * 60 * 60 * 1000);
+  
+    setInterval(() => {
+      this.decreaseStat('attention', 5);
+    }, 8 * 60 * 60 * 1000);
+  }
+  
+  decreaseStat(stat: StatName, decrement: number) {
+    let valueKey: keyof StatProperties;
+
     switch (stat) {
+        case 'hunger':
+            valueKey = 'hungerValue';
+            break;
+        case 'fatigue':
+            valueKey = 'fatigueValue';
+            break;
+        case 'purity':
+            valueKey = 'purityValue';
+            break;
+        case 'attention':
+            valueKey = 'attentionValue';
+            break;
+        default:
+            throw new Error(`Invalid stat: ${stat}`);
+    }
+
+    this[valueKey] = Math.max(this[valueKey] - decrement, 0);
+    this.checkAlert(stat);
+    this.updateContainerHeight(stat);
+    this.cdRef.markForCheck();
+}
+
+
+  // Update the next action time based on the stat
+  private updateNextActionTime(stat: StatName) {
+    const now = Date.now();
+    switch(stat) {
       case 'hunger':
-        this.hungerValue = Math.max(this.hungerValue - decrement, 0);
+        this.hungerNextAction = new Date(now + this.getRandomInterval(6, 12));
         break;
       case 'fatigue':
-        this.fatigueValue = Math.max(this.fatigueValue - decrement, 0);
+        this.fatigueNextAction = new Date(now + 24 * 60 * 60 * 1000);
         break;
       case 'purity':
-        this.purityValue = Math.max(this.purityValue - decrement, 0);
+        this.purityNextAction = new Date(now + 18 * 60 * 60 * 1000);
         break;
       case 'attention':
-        this.attentionValue = Math.max(this.attentionValue - decrement, 0);
+        this.attentionNextAction = new Date(now + 8 * 60 * 60 * 1000);
         break;
     }
-    this.cdRef.detectChanges();
   }
+
+   // Method to handle actions (e.g., feed, sleep) with timing checks and points assignment
+   handleAction(action: StatName) {
+    const now = new Date();
+    let nextActionTime : Date | undefined;
+    let incrementValue: number = 0;
+    
+    clearInterval(this.decreaseInterval);
+
+    switch (action) {
+      case 'hunger':
+        nextActionTime = this.hungerNextAction;
+        incrementValue = 20;
+        break;
+      case 'fatigue':
+        nextActionTime = this.fatigueNextAction;
+        incrementValue = 30;
+        break;
+      case 'purity':
+        nextActionTime = this.purityNextAction;
+        incrementValue = 20;
+        break;
+      case 'attention':
+        nextActionTime = this.attentionNextAction;
+        incrementValue = 25;
+        break;
+    }
+
+    this.increaseStat(action, incrementValue);
+
+   // Ensure nextActionTime is defined before proceeding
+  if (nextActionTime) {
+    const timeDifference = Math.abs(now.getTime() - nextActionTime.getTime()) / (60 * 1000);
+
+    if (timeDifference <= 10) {
+      this.addPoint(1);
+    } else if (now < nextActionTime) {
+      this.addPoint(0.5);
+    } else {
+      this.addPoint(0); 
+    }
+
+    // Update the next interval based on whether the action was early or late
+    const newInterval = this.getRandomInterval(6, 12);
+    nextActionTime.setTime(now.getTime() + newInterval);
+  } else {
+    console.warn(`nextActionTime is undefined for action: ${action}`);
+  }
+
+  this.increaseStat(action, incrementValue);
+}
+
+ // Increase stat helper method
+ increaseStat(stat: StatName, increment: number) {
+  const valueKey = `${stat}Value` as keyof StatProperties;
+  this[valueKey] = Math.min(this[valueKey] + increment, 100);
+  this.updateNextActionTime(stat);
+  //this.updateStatColors();
+  this.updateContainerHeight(stat);
+}
+
+updateCurrentStat(stat: StatName) {
+  const valueKey = `${stat}Value` as keyof StatProperties;
+  this.currentStatValue = this[valueKey];
+  this.currentStatColor = this.getStatusColor(this[valueKey] as number);
+}
+
+checkAlert(stat: StatName) {
+  const valueKey = `${stat}Value` as keyof StatProperties;
+  if (this[valueKey] < 10) {
+    alert(`Your pet's ${stat} is critically low!`);
+  }
+}
+
 
   getStatusColor(value: number): string {
     if (value >= 75) {
@@ -83,6 +241,17 @@ export class GamePage {
     }
   }
 
+  updateContainerHeight(stat: StatName) {
+    const valueKey = `${stat}Value` as keyof StatProperties;
+    const colorKey = `${stat}Color` as StatColorName; // Change here
+    this[colorKey] = this.getStatusColor(this[valueKey] as number);
+    this.cdRef.detectChanges(); 
+}
+
+  getContainerHeight(value: number): string {
+    return `${value}%`; 
+}
+
   updateColorGradually() {
     setInterval(() => {
       const currentHour = new Date().getHours();
@@ -90,53 +259,39 @@ export class GamePage {
       const colorValue = 255 - Math.floor((currentHour / 23) * 255); // Adjust as needed
       this.currentColor = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
       this.cdRef.detectChanges();
-    }, 3600000); // Update every hour
+    }, 2000); // Update every hour
   }
 
   bath() {
-    this.increaseStat('purity', 20);
-    this.addPoint();
+    this.handleAction('purity');
+    this.updateCurrentStat('purity');
   }
 
   sleep() {
-    this.increaseStat('fatigue', 30);
-    this.addPoint();
+    this.handleAction('fatigue');
+    this.updateCurrentStat('fatigue');
   }
 
   care() {
-    this.increaseStat('attention', 25);
-    this.addPoint();
+    this.handleAction('attention');
+    this.updateCurrentStat('attention');
   }
 
   feed() {
-    this.increaseStat('hunger', 20);
-    this.addPoint();
+    this.handleAction('hunger');
+    this.updateCurrentStat('hunger');
   }
 
-  // Method to increase the status by a fixed amount
-  increaseStat(stat: string, increment: number) {
-    switch(stat) {
-      case 'hunger':
-        this.hungerValue = Math.min(this.hungerValue + increment, 100);
-        break;
-      case 'fatigue':
-        this.fatigueValue = Math.min(this.fatigueValue + increment, 100);
-        break;
-      case 'purity':
-        this.purityValue = Math.min(this.purityValue + increment, 100);
-        break;
-      case 'attention':
-        this.attentionValue = Math.min(this.attentionValue + increment, 100);
-        break;
-    }
+  setNextFeedTime() {
+    const now = new Date();
+    const randomHours = Math.floor(Math.random() * 6) + 6; // 6 do 12 sati
+    this.nextFeedTime = new Date(now.getTime() + randomHours * 60 * 60 * 1000);
   }
 
-  // Method to add points for leveling
-  addPoint() {
-    this.points += 1;
-
+  // Adds points based on user timing
+  addPoint(points: number) {
+    this.points += points;
     this.progressBarWidth = (this.points / this.pointsNeeded) * 100;
-
     if (this.points >= this.pointsNeeded) {
       this.levelUp();
     }
@@ -144,10 +299,16 @@ export class GamePage {
 
   // Method to handle leveling up
   levelUp() {
-    this.level += 1;        
-    this.points = 0;      
-    this.pointsNeeded = Math.ceil(this.pointsNeeded * 1.1); 
-    this.progressBarWidth = 0;
+    if (this.level < 50) { 
+      this.level += 1;        
+      this.points = 0;
+      this.pointsNeeded = Math.ceil(this.pointsNeeded * 1.1); 
+      this.progressBarWidth = 0;
+    } else {
+      this.points = this.pointsNeeded;
+      this.progressBarWidth = 100;
+      console.log("Maximum level reached.");
+    }
   }
 
   makeDogJump() {
