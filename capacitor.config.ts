f7986c66200c2edc66253cdb10c2d0c0007e6cc8
majapremuz <majchi@windowslive.com>
2025-03-14 13:42:27 +0100

    promijenila appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'dog.game',
+  appId: 'com.prolink.fetchthebone',
   appName: 'Fetch the bone',
   webDir: 'www',
 
