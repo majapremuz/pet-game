c45804a612ee0c1623ca6e35dcf71e4cb75163f6
majapremuz <majchi@windowslive.com>
2025-06-09 13:14:07 +0200

    changed bundle name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.prolink.fetchthebone',
+  appId: 'com.zita.fetchthebone',
   appName: 'Fetch the bone',
   webDir: 'www',
 
